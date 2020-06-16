d89fecf90c74ab0c351525fc12ddfb76f8bc7f41
robinvanputten <r.vanputten@student.bit-academy.nl>
2020-06-16 21:23:04 +0200

    Removed unnecessary arguments

--- data_anonymizer/__main__.py
+++ data_anonymizer/__main__.py
@@ -7,13 +7,12 @@ from .ConfigReader import config
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('-c', '--config', dest='configfile')
-    parser.add_argument('--host', dest='host')
-    parser.add_argument('--user', dest='username')
-    parser.add_argument('--pass', dest='password')
-    parser.add_argument('--db', dest='database')
-    parser.add_argument('-i', '--input', dest='inputfile')
-    parser.add_argument('-o', '--output', dest='outputfile')
+    parser.add_argument('-c', '--config', dest='configfile', 
+                        required=True, help="Configuration file")
+    parser.add_argument('-i', '--input', dest='inputfile', 
+                        required=True, help="SQL dump from database")
+    parser.add_argument('-o', '--output', dest='outputfile', 
+                        required=True, help="Anonymized SQL dump")
 
     args = parser.parse_args()
 
@@ -25,25 +24,16 @@ if __name__ == '__main__':
         print(str(infile) + " is not a valid file")
         sys.exit()
 
-    # if not os.path.isfile(configfile):
-    #     print(str(configfile) + " is not a valid file")
-    #     sys.exit()
+    if not os.path.isfile(configfile):
+        print(str(configfile) + " is not a valid file")
+        sys.exit()
 
-    # configreader = config(open(configfile, 'r'))
+    configreader = config(open(configfile, 'r'))
 
-    # host = configreader.storage()['host']
-    # username = configreader.storage()['username']
-    # password = configreader.storage()['password']
-    # database = configreader.storage()['database']
-
-    if args.host is not None:
-        host = args.host
-    if args.username is not None:
-        username = args.username
-    if args.password is not None:
-        password = args.password
-    if args.database is not None:
-        database = args.database
+    host = configreader.storage()['host']
+    username = configreader.storage()['username']
+    password = configreader.storage()['password']
+    database = configreader.storage()['database']
 
     anonymizer = data.Anonymize(host=host, username=username, 
                                 password=password, database=database, 
